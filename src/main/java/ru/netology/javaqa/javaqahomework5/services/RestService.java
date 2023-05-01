50c96f39acc9caf30ab4fb9c5526a33a35f76415
Екатерина Кобелева <kobelevaekaterina09@gmail.com>
2023-05-01 20:05:28 +0300

    Changed project

--- src/main/java/ru/netology/javaqa/javaqahomework5/services/RestService.java
+++ src/main/java/ru/netology/javaqa/javaqahomework5/services/RestService.java
@@ -8,7 +8,7 @@ public class RestService {
             if (cash >= threshold) { // можем ли отдыхать?
                 months++; // увеличиваем счётчик месяцев отдыха
                 System.out.println("Месяц " + month + " Денег " + cash + " Отдыхаем " + " Потратил -" + expenses + " Затем ещё -" + (cash - expenses - (cash - expenses) / 3));
-                cash = cash - expenses - (cash - expenses - (cash - expenses) / 3);
+                cash = (cash - expenses) / 3;
             } else {
                 System.out.println("Месяц " + month + " Денег " + cash + " Придется работать. Заработал +" + income + " Потратил -" + expenses);
                 cash = cash + income - expenses;
